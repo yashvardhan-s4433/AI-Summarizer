e3b8f081753fba23e48959cd0bc97abcd1e6899c
Yashvardhan Singh <130577344+yashvardhan-s4433@users.noreply.github.com>
2025-03-14 02:26:31 +0530

    Update api.js

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,1 +1,24 @@
+import axios from "axios";
+
+const API_URL = "http://127.0.0.1:5000";
+
+export const summarizeText = async (text) => {
+    const response = await axios.post(`${API_URL}/summarize`, { text });
+    return response.data;
+};
+
+export const summarizeURL = async (url) => {
+    const response = await axios.post(`${API_URL}/summarize-url`, { url });
+    return response.data;
+};
+
+export const summarizePDF = async (file) => {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const response = await axios.post(`${API_URL}/summarize-pdf`, formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+    });
+    return response.data;
+};
 
